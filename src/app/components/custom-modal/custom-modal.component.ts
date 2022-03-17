3eb2b758c3bd5b15f4d25269637694c7f1a3b617
Gowriekaran Sinnadurai <gowriekaran@users.noreply.github.com>
2022-03-17 02:29:51 -0400

    v1.0.0
    Modal structure update; evolutions are now included and clickable.

--- src/app/components/custom-modal/custom-modal.component.ts
+++ src/app/components/custom-modal/custom-modal.component.ts
@@ -1,29 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Pokemon } from 'src/app/shared/models/Pokemon';
-import { Modal } from 'src/app/service/modal/modal.service';
+import {Component, OnInit} from '@angular/core';
+import {Pokemon} from 'src/app/shared/models/Pokemon';
+import {Modal} from 'src/app/service/modal/modal.service';
+import {PokemonService} from 'src/app/service/pokemon/pokemon.service';
 
-@Component({
-  selector: 'app-custom-modal',
-  templateUrl: './custom-modal.component.html',
-  styleUrls: ['./custom-modal.component.scss'],
-})
-export class CustomModalComponent implements OnInit {
-  visible = false;
-  pokemon!: Pokemon;
-  message!: '';
-  constructor(private modal: Modal) {}
+@Component({selector: 'app-custom-modal', templateUrl: './custom-modal.component.html', styleUrls: ['./custom-modal.component.scss']})export class CustomModalComponent implements OnInit {
+    visible = false;
+    pokemon !: Pokemon;
+    message !: '';
+    hasEvolution = false;
+    evolutionFamily !: any[];
+    constructor(private modal : Modal, private pokemonService : PokemonService) {}
 
-  ngOnInit(): void {
-    this.modal.getModalRequest().subscribe((pokemon) => {
-      if (pokemon) {
-        this.visible = true;
-        this.pokemon = pokemon;
-      }
-    });
-  }
+    ngOnInit(): void {
+        this.modal.getModalRequest().subscribe((pokemon) => {
+            if (pokemon) {
+                this.visible = true;
+                this.pokemon = pokemon;
+                if (this.pokemon.hasEvolution) {
+                    this.hasEvolution = true;
+                    this.evolutionFamily = this.pokemonService.getAllPokemonsbyEvolutionFamilyId(this.pokemon.evolutionFamilyId, this.pokemon.id);
+                }
+            }
+        });
+    }
 
-  hideModal(): void {
-    this.visible = false;
-    this.pokemon = new Pokemon();
-  }
+    hideModal(): void {
+        this.visible = false;
+        this.pokemon = new Pokemon();
+    }
 }
--- src/app/shared/models/Pokemon.ts
+++ src/app/shared/models/Pokemon.ts
@@ -1,9 +1,11 @@
 export class Pokemon {
-  id!: number;
-  name!: string;
-  types!: Array<string>;
-  height!: number;
-  weight!: number;
-  description!: string;
-  imageURL!: string;
+    id !: number;
+    name !: string;
+    types !: Array < string >;
+    height !: number;
+    weight !: number;
+    description !: string;
+    imageURL !: string;
+    hasEvolution !: boolean;
+    evolutionFamilyId !: number;
 }
